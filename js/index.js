7dd1b524eff5a9638227f46e2650740656b57301
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-03-30 00:03:59 -0500

    lab

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@
   greetProgrammer();
   => "Hello, programmer!"
 */
-function greetProgrammer() {
-  console.log("Hello, programmer!");
-}
+def greet_programmer():
+    print("Hello, programmer!")
 
 /*
   You should be able to call this function with one argument and see its output in the terminal:
